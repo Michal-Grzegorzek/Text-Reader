a3edb491b242737589fff245a17e9c70c4893022
Mike <michal.grzegorzek33@gmail.com>
2023-01-12 20:26:01 +0100

    adding requirements.txt, debugging buttons save, select image

--- main.py
+++ main.py
@@ -21,34 +21,34 @@ root.wm_iconbitmap('favicon.ico')
 root.geometry("620x350")  # Size of the window
 root.title('Text Reader')
 
-# Wczytaj zdjęcie
+# Upload photo
 image = Image.open('Text_Reader_background.png')
-# Przekonwertuj zdjęcie na obiekt PhotoImage
+# Convert the photo to an object PhotoImage
 image = ImageTk.PhotoImage(image)
 
-# Utwórz widget Label z zdjęciem
+# Create a Label widget with a photo
 label = ttk.Label(root, image=image)
 
-# Wyświetl widget Label
+# View the Label widget
 label.pack()
 
 
-def upload_image():
+def upload_document():
     global pdf_path
     pdf_path = filedialog.askopenfilename(initialdir=os.getcwd())
-    print(pdf_path)
-    if os.path.splitext(pdf_path)[1] != '.pdf':
+
+    if pdf_path == '':
+        pass
+    elif os.path.splitext(pdf_path)[1] != '.pdf':
         messagebox.showinfo(title="Pdf file not detected.", message="This is not a PDF file.")
         pdf_path = None
-    elif pdf_path != '':
+    else:
         messagebox.showinfo(title="Success", message="Success file uploaded.")
-
-
 
 
 def check_type_start():
     selected = choose_type.get()
-    b2['text'] = 'PAUSE'
+    btn_pause['text'] = 'PAUSE'
 
     if selected == 'PDF':
         pdf_doc()
@@ -58,25 +58,19 @@ def check_type_start():
         messagebox.showinfo(title="Choose Type", message="Choose type: Text or PDF.")
 
 
-
-
-# Funkcja obsługi zdarzenia
 def on_choose_type_select(event):
-    # Pobierz aktualnie wybraną opcję
     selected = choose_type.get()
 
-    # Jeśli opcja to 'PDF', umożliw kliknięcie przycisku
     if selected == 'PDF':
-        btn_select_image['state'] = 'normal'
+        btn_select_document['state'] = 'normal'
         enter_text.delete(0, tk.END)
         enter_text['state'] = 'disabled'
     elif selected == 'Text':
         enter_text['state'] = 'normal'
         enter_text.insert(0, 'Enter Text')
-        btn_select_image['state'] = 'disabled'
-    # W przeciwnym razie deaktywuj przycisk
+        btn_select_document['state'] = 'disabled'
     else:
-        btn_select_image['state'] = 'disabled'
+        btn_select_document['state'] = 'disabled'
         enter_text['state'] = 'disabled'
 
 
@@ -86,7 +80,8 @@ def on_choose_language_select(event):
 
 
 def clear_entry(event):
-    enter_text.delete(0, 'end')
+    if enter_text.get() == 'Enter Text':
+        enter_text.delete(0, 'end')
 
 
 def text_doc():
@@ -106,25 +101,20 @@ def text_doc():
 
 
 def pdf_doc():
+    global tts
 
-    if pdf_path == None:
+    if pdf_path is None:
         messagebox.showinfo(title="Select a document", message="First select a document.")
     else:
         pygame.init()
         pygame.mixer.init()
         with open(pdf_path, 'rb') as f:
-            # Wczytaj plik PDF
             pdf_reader = PyPDF2.PdfReader(f)
-            # Pobierz wszystkie arkusze
             pages = pdf_reader.pages
-            # Utwórz tekst z wszystkich arkuszy
             text = ''
             for page in pages:
                 text += page.extract_text()
-            # Utwórz obiekt gTTS z tekstem
             tts = gTTS(text, lang=language)
-            # Zapisz mowę syntetyczną do pliku MP3
-            # tts.save('document.mp3')
             mp3_fo = BytesIO()
             tts.write_to_fp(mp3_fo)
             pygame.mixer.music.load(mp3_fo, 'mp3')
@@ -133,25 +123,25 @@ def pdf_doc():
 
 def pause_unpause():
 
-    if b2['text'] == 'PAUSE':
+    if btn_pause['text'] == 'PAUSE':
         pygame.mixer.music.pause()
-        b2['text'] = 'UNPAUSE'
+        btn_pause['text'] = 'UNPAUSE'
     else:
         pygame.mixer.music.unpause()
-        b2['text'] = 'PAUSE'
+        btn_pause['text'] = 'PAUSE'
 
 
 def reset():
     global pdf_path, tts
     pygame.mixer.music.stop()
-    b2['text'] = 'PAUSE'
+    btn_pause['text'] = 'PAUSE'
     pdf_path = None
     enter_text.delete(0, 'end')
     tts = None
 
 
 def save():
-    if tts != None:
+    if tts is not None:
         filename = filedialog.asksaveasfilename(initialdir=os.getcwd())
         if filename:
             tts.save(f'{filename}.mp3')
@@ -159,21 +149,15 @@ def save():
         messagebox.showinfo(title="Start", message="Press first start.")
 
 
-# Stwórz obiekt Combobox z dwoma opcjami: 'pdf' i 'mój własny tekst'
 choose_type = ttk.Combobox(root, values=['PDF', 'Text'], state='readonly', width=20)
 choose_type.set('Choose Type')
-# choose_type.pack(side=LEFT, padx=5, pady=10)
 choose_type.place(x=5, y=275)
-
-# Nasłuchuj zdarzenia <<ComboboxSelected>>
 choose_type.bind("<<ComboboxSelected>>", on_choose_type_select)
 
 
 choose_language = ttk.Combobox(root, values=['en', 'de', 'fr', 'pl', 'es'], state='readonly', width=20)
 choose_language.set('Choose Language')
 choose_language.place(x=160, y=275)
-
-# Nasłuchuj zdarzenia <<ComboboxSelected>>
 choose_language.bind("<<ComboboxSelected>>", on_choose_language_select)
 
 
@@ -182,23 +166,24 @@ enter_text.place(x=315, y=275)
 enter_text.bind('<Button-1>', clear_entry)
 
 
-btn_select_image = ttk.Button(root, text="Select Document", width=20, bootstyle=INFO, state='disabled', command=upload_image)
-btn_select_image.place(x=470, y=275)
+btn_select_document = ttk.Button(root, text="Select Document", width=20, bootstyle=INFO, state='disabled',
+                                 command=upload_document)
+btn_select_document.place(x=470, y=275)
 
 
-b1 = ttk.Button(root, text="START", bootstyle=SUCCESS, command=check_type_start, width=20)
-b1.place(x=5, y=310)
+btn_start = ttk.Button(root, text="START", bootstyle=SUCCESS, command=check_type_start, width=20)
+btn_start.place(x=5, y=310)
 
 
-b2 = ttk.Button(root, text="PAUSE", bootstyle=(SECONDARY, OUTLINE), command=pause_unpause, width=20)
-b2.place(x=160, y=310)
+btn_pause = ttk.Button(root, text="PAUSE", bootstyle=(SECONDARY, OUTLINE), command=pause_unpause, width=20)
+btn_pause.place(x=160, y=310)
 
 
-b3 = ttk.Button(root, text="RESET", bootstyle=(DANGER, OUTLINE), command=reset, width=20)
-b3.place(x=315, y=310)
+btn_reset = ttk.Button(root, text="RESET", bootstyle=(DANGER, OUTLINE), command=reset, width=20)
+btn_reset.place(x=315, y=310)
 
 
-b4 = ttk.Button(root, text="SAVE", bootstyle=(WARNING, OUTLINE), command=save, width=20)
-b4.place(x=470, y=310)
+btn_save = ttk.Button(root, text="SAVE", bootstyle=(WARNING, OUTLINE), command=save, width=20)
+btn_save.place(x=470, y=310)
 
 root.mainloop()
